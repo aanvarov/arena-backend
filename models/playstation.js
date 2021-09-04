2688c4f09f0f187334b75ffc415f43f655045ff7
aanvarov <anvarov2295@gmail.com>
2021-09-04 16:39:43 +0500

    playstation model added

---
+++ models/playstation.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const mongoosePaginate = require("mongoose-paginate-v2");
+
+const playstationSchema = new Schema({
+  number: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    default: "ps3",
+    enum: ["ps3", "ps4", "ps5"],
+  },
+  status: {
+    type: String,
+    default: "free",
+    enum: ["free", "busy"],
+  },
+  hourlyPrice: {
+    type: Number,
+    default: 6000,
+  },
+  totatTime: {
+    type: String,
+    default: "",
+  },
+  totalEarning: {
+    type: Number,
+    default: 0,
+  },
+});
+
+playstationSchema.plugin(mongoosePaginate);
+const Playstations = mongoose.model("Playstation", playstationSchema);
+
+module.exports = Playstations;
