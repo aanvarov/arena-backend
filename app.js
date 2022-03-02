e32819597e2fcc28acd7754536a1e59bd63a1dbe
aanvarov <anvarov2295@gmail.com>
2022-03-02 14:27:10 +0500

    root endpoint added

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
-const mongoose = require("mongoose");
-const cors = require("cors");
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const port = process.env.PORT || 7007;
 // local mongodb database
-const localDatabase = "mongodb://localhost/arena";
+const localDatabase = 'mongodb://localhost/arena';
 
-const playstationRouter = require("./routes/playstations");
-const foodRouter = require("./routes/foods");
-const adminRouter = require("./routes/admins");
-const authRouter = require("./routes/auth");
-const dayRouter = require("./routes/days");
-const clubRouter = require("./routes/clubs");
-const orderRouter = require("./routes/orders");
+const playstationRouter = require('./routes/playstations');
+const foodRouter = require('./routes/foods');
+const adminRouter = require('./routes/admins');
+const authRouter = require('./routes/auth');
+const dayRouter = require('./routes/days');
+const clubRouter = require('./routes/clubs');
+const orderRouter = require('./routes/orders');
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   cors({
     credentials: true,
     origin: true,
-  })
+  }),
 );
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
-  })
+  }),
 );
 
-app.use("/auth", authRouter);
-app.use("/playstations", playstationRouter);
-app.use("/foods", foodRouter);
-app.use("/admins", adminRouter);
-app.use("/days", dayRouter);
-app.use("/clubs", clubRouter);
-app.use("/orders", orderRouter);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello World</h1>');
+});
+
+app.use('/auth', authRouter);
+app.use('/playstations', playstationRouter);
+app.use('/foods', foodRouter);
+app.use('/admins', adminRouter);
+app.use('/days', dayRouter);
+app.use('/clubs', clubRouter);
+app.use('/orders', orderRouter);
 
 mongoose
   .connect(localDatabase, {
@@ -47,8 +51,8 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("LocalDatabase is connected");
+    console.log('LocalDatabase is connected');
   })
   .catch((error) => console.log(error));
 
-server.listen(port, () => console.log("Server is running on port: ", port));
+server.listen(port, () => console.log('Server is running on port: ', port));
